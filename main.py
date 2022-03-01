a9badf622e822b953cc041ba93a871cf04ba2ddf
Lucas Costa de Souza <94208894+lucascostastz@users.noreply.github.com>
2022-03-01 16:55:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from PyQt6 import uic,QtWidgets
-import pyautogui 
+from PyQt6 import uic, QtWidgets
+import pyautogui
 from selenium import webdriver
 import time
 
 
 def comecar():
+
     # Abrir navegador
     navegador = webdriver.Chrome("chromedriver")
     # Abrir link do instagram
@@ -15,47 +16,50 @@ def comecar():
 
     # Digitar nome de usuário
     usuario = primeira_tela.lineEdit_2.text()
-    navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(usuario)
+    navegador.find_element_by_xpath(
+        '//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(usuario)
 
     # Esperar 1 segundos
     time.sleep(1)
 
-     # Digitar senha
+    # Digitar senha
     senha = primeira_tela.lineEdit_3.text()
-    navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(senha)
+    navegador.find_element_by_xpath(
+        '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(senha)
 
     # Esperar 1 segundo
     time.sleep(1)
 
-    #Clicar no botão entrar
-    navegador.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
+    # Clicar no botão entrar
+    navegador.find_element_by_xpath(
+        '//*[@id="loginForm"]/div/div[3]/button/div').click()
 
-    # Tempo 
+    # Tempo
     time.sleep(3)
 
     # Clicar no botão agora não
-    navegador.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+    navegador.find_element_by_xpath(
+        '//*[@id="react-root"]/section/main/div/div/div/div/button').click()
 
     # Tempo
     time.sleep(3)
     # Clicar no botão de desaivar notificações
-    navegador.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()
+    navegador.find_element_by_xpath(
+        '/html/body/div[5]/div/div/div/div[3]/button[2]').click()
 
-    # Tempo
+    # Tempo+
     time.sleep(3)
 
     pyautogui.press('F6')
 
     time.sleep(1)
-    link = primeira_tela.lineEdit_4.text()
+    link = primeira_tela.Inserir_Link.text()
 
     pyautogui.write(link)
     time.sleep(1)
     pyautogui.press('Enter')
 
 
-
-    
 # Clicar em Comentar
     navegador.find_element_by_class_name("Ypffh").click()
     # Tempo
@@ -67,46 +71,37 @@ def comecar():
 
     # Tempo
     time.sleep(1)
-    pyautogui.press('Enter') 
+    pyautogui.press('Enter')
 
 
 ############################# TEMPO #############################
+
     def tempo():
-# Tempo Selecionado
+        # Tempo Selecionado
         tempo = primeira_tela.comboBox.currentText()
         time.sleep(int(tempo)*(60))
-        
+
         # Comentar
         comentario = primeira_tela.lineEdit_5.text()
         navegador.find_element_by_class_name("Ypffh").send_keys(comentario)
         pyautogui.press('Enter')
-    tempo()       
-    while True:  
-        tempo() 
+    tempo()
+    while True:
+        tempo()
 
 
-        
-        
-
-        
-        
 def sair():
-            primeira_tela.close()
-        
+    primeira_tela.close()
 
-app=QtWidgets.QApplication([])
-primeira_tela=uic.loadUi("tela.ui")
+
+app = QtWidgets.QApplication([])
+primeira_tela = uic.loadUi("tela.ui")
 
 primeira_tela.pushButton_2.clicked.connect(comecar)
-primeira_tela.pushButton_3.clicked.connect(sair) 
-primeira_tela.comboBox.addItems(["1","2","3","4","5","6","7","8","9","10"])
-""" primeira_tela.pushButton_4.clicked.connect(tempo)
- """
-
-
-comecar
+primeira_tela.pushButton_3.clicked.connect(sair)
+primeira_tela.comboBox.addItems(
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
 
 
 primeira_tela.show()
-
 app.exec()
